667341669b25d533ddea1279f01ed93377ce49df
Maxim Yefremov <maxYefr@gmail.com>
2018-05-01 18:39:46 +0300

    remove target on rebuild

--- commands.sh
+++ commands.sh
@@ -2,11 +2,12 @@
 set -ex
 
 build(){
+	rm -rf ./target
 	mvn package
 }
 
 run(){
-	cat 4_big.txt | java -cp target/testlog4j-1.0-jar-with-dependencies.jar testlog4j.testlog4j.App
+	java -cp target/testlog4j-1.0-jar-with-dependencies.jar testlog4j.testlog4j.App
 }
 
 br(){

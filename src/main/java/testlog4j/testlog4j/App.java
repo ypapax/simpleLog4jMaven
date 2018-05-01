fb52343a064c7107c08adbb95dc26ebefc66c86c
Maxim Yefremov <maxYefr@gmail.com>
2018-05-01 18:33:43 +0300

    removed commented out code

--- src/main/java/testlog4j/testlog4j/App.java
+++ src/main/java/testlog4j/testlog4j/App.java
@@ -12,7 +12,6 @@ import org.apache.log4j.*;
 import org.apache.log4j.xml.*;
 
 public class App {
-    // Next : check jar 
     static Logger logger = Logger.getLogger(App.class);
 
     public static void main(String[] args) {
